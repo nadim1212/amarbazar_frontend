f63351c767581c840145690d308bc7b8d3b0920b
nadim1212 <nadimniloy.swe@gmail.com>
2023-09-14 18:37:08 +0600

    payment

--- src/components/Shop/Layout/DashboardHeader.jsx
+++ src/components/Shop/Layout/DashboardHeader.jsx
@@ -5,7 +5,7 @@ import { FiPackage, FiShoppingBag } from "react-icons/fi";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { BiMessageSquareDetail } from "react-icons/bi";
-import style from "../../styles/style.css";
+
 const DashboardHeader = () => {
   const { seller } = useSelector((state) => state.seller);
   return (
