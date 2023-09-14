7c7968cdf46c73af280142f678b4962bdb905f46
nadim1212 <nadimniloy.swe@gmail.com>
2023-09-14 22:42:41 +0600

    symbol

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -428,7 +428,7 @@ const CartData = ({ orderData }) => {
         <h5 className="text-[18px] font-[600]">{orderData?.discountPrice? "৳" + orderData.discountPrice : "-"}</h5>
       </div>
       <h5 className="text-[18px] font-[600] text-end pt-3">
-        ${orderData?.totalPrice}
+      ৳{orderData?.totalPrice}
       </h5>
       <br />
     </div>
